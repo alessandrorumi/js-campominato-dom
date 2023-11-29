f8202c2b057af37a87f41b0a795c3f88dd8929bd
alessandrorumi <rumi.alessandro21@gmail.com>
2023-11-29 14:42:07 +0100

    Feature Bonus: Stop interazione; Style: Grid

--- Bonus/script/myScript.js
+++ Bonus/script/myScript.js
@@ -14,8 +14,18 @@ const stop = document.getElementById('reset');
 // Dichiarazione scleta Difficoltà
 const userChoice = document.getElementById('choice');
 
-// Dichiaraione elementi
-  let itemsGenerated = 0;
+  // Creazione 100 divs
+  let itemsGenerated = 0; 
+  
+  // Numero di Bombe
+  let bombs = generateArrayRandom(1, 100, 16);
+  console.log(bombs);
+
+  // Reset punteggio
+  let score = 0;
+
+  // Dichiaro la variabile 'Sto ancora giocando'
+  let didNotLose = true;
 
 // Al click del bottone play
 playButton.addEventListener('click', function() {
@@ -52,7 +62,7 @@ playButton.addEventListener('click', function() {
     const myNewElement = createMyElement('div', 'square');
 
     // Append contenuto
-    myNewElement.append(i);
+    // myNewElement.append(i);
 
     // Larghezza di ogni riga in %
     myNewElement.style.width = `${columnWidth}%`;
@@ -62,10 +72,29 @@ playButton.addEventListener('click', function() {
 
     // Al click del div .square
     myNewElement.addEventListener('click', function() {
-    
-      myNewElement.classList.add('clicked');
 
-      console.log([i]);
+      // Se non ho ancora perso
+      if (didNotLose) {
+
+        // Se "le bombe" hanno valore = al numero della variabile i
+        if (bombs.includes(i)) {
+          // Bomba
+          myNewElement.classList.add('dead');
+  
+          alert(`Hai perso, il tuo punteggio è ${score}`);
+  
+          // Reimposta il punteggio quando si perde
+          score = 0;
+  
+          didNotLose = false;
+  
+        } else {
+          // Sei salvo
+          myNewElement.classList.add('save');
+          score++;
+        }
+
+      }
     
     });
     
@@ -78,6 +107,7 @@ stop.addEventListener('click', function() {
 
   // Creazione griglia vuota
   mainContainer.innerHTML = '';
+  didNotLose = true;
 
 })
 
@@ -89,3 +119,27 @@ function createMyElement(htmlElement, className) {
 
   return newElement;
 };
+
+// Funzione Generazione numeri in array (in modo casuale)
+function generateArrayRandom(min, max, arrayLength) {
+
+  // Array vuoto
+  arrayRandom = [];
+
+  // Finchè gli elementi nell'array sono uguali al n° di elementi
+  while (arrayRandom.length < arrayLength) {
+    
+    // Genera n° random
+    const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    // Se il numero random non è presente nell'array
+    if (!arrayRandom.includes(randomNumber)) {
+
+      // Push nell'array
+      arrayRandom.push(randomNumber);
+    }
+  }
+
+  // Restituisci l'array
+  return arrayRandom;
+}
